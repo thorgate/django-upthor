94605042885ee7529f26c6d04da68d71f5601fd2
Jorgen Ader <jorgen@thorgate.eu>
2016-06-29 22:23:58 +0300

    fixed ThorImageField allowed_types defined multiple times

--- upthor/fields.py
+++ upthor/fields.py
@@ -209,10 +209,9 @@ class ThorImageField(ThorFileField):
     descriptor_class = ImageFileDescriptor
     description = _("Image")
 
-    def __init__(self, verbose_name=None, name=None, width_field=None,
-            height_field=None, **kwargs):
+    def __init__(self, verbose_name=None, name=None, width_field=None, height_field=None, **kwargs):
         self.width_field, self.height_field = width_field, height_field
-        super(ThorImageField, self).__init__(verbose_name, name, **kwargs)
+        super(ThorImageField, self).__init__(verbose_name=verbose_name, name=name, **kwargs)
 
     @staticmethod
     def get_widget_class():
